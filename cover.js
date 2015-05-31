b08fff652c0943d0f461afe7f37b3c5c7ee3a723
ElenoreChang <zhangxiaohan_elenore@hotmail.com>
2015-05-31 14:32:48 +0800

    Add JSON

---
+++ cover.js
@@ -0,0 +1,19 @@
+window.onload= function(){
+	var xmlhttp;
+	if (window.XMLHttpRequest){     // code for IE7+, Firefox, Chrome, Opera, Safari
+  		xmlhttp=new XMLHttpRequest();
+  	}
+	else{// code for IE6, IE5
+  		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  	}
+  	xmlhttp.open("GET","JSON.txt",true);
+  	xmlhttp.send();
+	xmlhttp.onreadystatechange=function(){
+  		if(xmlhttp.readyState==4){	
+  			var jsonResp=request.responseText;
+    		jsonResp=eval("("+jsonResp+")");
+    		alert(user.name+"的爱好是："+user.slogan); 
+    	}
+  	}  
+
+}
